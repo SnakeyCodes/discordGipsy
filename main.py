1f012bee4e1b59ca5432abba6cc937a293634c05
SnakeyCodes <63118005+SnakeyCodes@users.noreply.github.com>
2022-01-22 21:26:02 +0100

    Add files via upload

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
 import discord
+import random
+
+words = []
+with open("words.bot", "r") as wordsFile:
+    words = wordsFile.read().split("\n")
 
 with open("data.bot", "r") as dataFile:
     TOKEN = dataFile.read()
@@ -8,6 +13,7 @@ class Client(discord.Client):
     @staticmethod
     async def on_ready():
         print("Bot is logged in.")
+        print(f"Logoff word is : \"{word}\"")
         await client.get_channel(842001152068419584).send("Hi. I am gipsy. I am a bot developed for this server, " +
                                                           "type /info to learn more about me.")
 
@@ -20,9 +26,13 @@ class Client(discord.Client):
             responses = responsesF.read()
             responses = responses.replace("\n", "%")
             responses = responses.split("%")
+        if message.content == word:
+            await client.close()
+            exit()
         for index, x in enumerate(responses):
             if responses[index] == message.content.lower():
                 if responses[index + 1][0:2] == "!p":
+                    print("Sending: " + responses[index + 1][2:])
                     await message.channel.send(responses[index + 1][2:], reference=message)
                     return
                 print("Sending: " + responses[index + 1])
@@ -32,6 +42,7 @@ class Client(discord.Client):
 
 client = Client()
 try:
+    word = random.choice(words)
     client.run(TOKEN)
 except:
     pass
